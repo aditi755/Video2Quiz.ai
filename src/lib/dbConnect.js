d1e7009d0221bdff678a88486f1a744a317d34fc
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-08-05 14:41:47 +0530

    updated dbconnect

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -24,3 +24,30 @@ async function dbConnect() {
 
 export default dbConnect;
 
+
+// import mongoose from 'mongoose';
+
+// // Fallback MongoDB URI
+// const DEFAULT_MONGO_URI = 'mongodb+srv://awdhesh1700:taWitFufuDSQLTUj@cluster0.nmqnlcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// const MONGO_URI = process.env.MONGO_URI || DEFAULT_MONGO_URI;
+// console.log("Using MongoDB URI:", MONGO_URI);
+
+// async function dbConnect() {
+//   if (mongoose.connections[0].readyState) return;
+
+//   try {
+//     await mongoose.connect(MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('Database connected successfully');
+//   } catch (error) {
+//     console.error('Error connecting to database:', error);
+//     throw error;
+//   }
+// }
+
+// export default dbConnect;
+
+
